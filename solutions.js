4907017c19c31158d1af6352dd7563dbe3381730
Quentin Hewitt <quentin.l.hewitt@gmail.com>
2021-06-10 08:57:34 -0500

    practice

--- solutions.js
+++ solutions.js
@@ -1,2 +1,41 @@
 // Write your Solutions here!!
+function onMarsRightNow () {
+    return false;
+}
+function doingJavaScriptRightNow () {
+    return true;
+}
+function sayHello(name) {
+    if (typeof name === "string") {
+        return "Hello, " + name + "!";
+    } else {
+        return "Hello, world!"
+    }
+}
 
+function addOne (x, y) {
+    if (x+y != isNaN() )
+        x=1,   y= x++
+    return (x + y);
+}
+
+
+function plusTwo (x, y) {
+    if (x+y != isNaN() )
+        y= x++
+    x=2
+    return (x + y);
+}
+
+
+
+
+function isPositive(x) {
+    if ( x > 0) {
+        return true;
+    } else {
+        return false;
+    }if (x = isNaN() || "" || undefined) {
+        return isNaN();
+    }
+}
